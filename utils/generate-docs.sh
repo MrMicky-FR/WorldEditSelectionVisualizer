f8cfd0eb35587c400f037af3f0c88d70f04190bd
Martin Ambrus <martin@martinambrus.com>
2017-03-04 20:56:13 +0100

    docs: automatic JavaDoc creation and upload to GitHub Page

---
+++ utils/generate-docs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] ; then
+
+  echo -e "Generating javadoc...\n"
+  ./gradlew Javadoc
+
+  echo -e "Publishing javadoc...\n"
+
+  cp -R build/com.rojel.wesv/docs/javadoc $HOME/javadoc-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/WPIRoboticsProjects/grip gh-pages #> /dev/null
+
+  cd gh-pages
+  git rm -rf ./javadoc
+  cp -Rf $HOME/javadoc-latest ./javadoc
+  git add -f .
+  git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq origin gh-pages #> /dev/null
+
+  echo -e "Published Javadoc to gh-pages.\n"
+
+fi
