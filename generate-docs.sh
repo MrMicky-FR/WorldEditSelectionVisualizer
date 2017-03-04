09251334dda1b3a30ec551fa4661d4d84cd1bc87
Martin Ambrus <martin@martinambrus.com>
2017-03-04 22:47:54 +0100

    docs: oops, forgot one more copy command...

--- generate-docs.sh
+++ generate-docs.sh
@@ -18,6 +18,7 @@ if [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "fal
   cd gh-pages
   git rm -rf ./javadoc
   cp -Rf $HOME/javadoc-latest ./javadoc
+  cp $HOME/README.md .
   git add -f .
   git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
