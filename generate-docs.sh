89f4b7c2b45cf49e6e23f95da884c34c67dd6bf5
Martin Ambrus <martin@martinambrus.com>
2017-03-04 21:24:14 +0100

    fix: more JavaDoc fixes

--- generate-docs.sh
+++ generate-docs.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] ; then
 
   echo -e "Generating javadoc...\n"
-  gradlew Javadoc
+  ./gradlew Javadoc
 
   echo -e "Publishing javadoc...\n"
 
