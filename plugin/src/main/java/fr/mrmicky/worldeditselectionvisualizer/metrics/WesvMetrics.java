cac0d47807aad44a2885634902177c03b2455a38
MrMicky <git@mrmicky.fr>
2019-07-23 17:04:28 +0200

    Add more metrics

--- plugin/src/main/java/fr/mrmicky/worldeditselectionvisualizer/metrics/WesvMetrics.java
+++ plugin/src/main/java/fr/mrmicky/worldeditselectionvisualizer/metrics/WesvMetrics.java
@@ -15,6 +15,8 @@ public final class WesvMetrics {
 
         CompatibilityHelper compatHelper = plugin.getCompatibilityHelper();
 
+        addObjectChart("selection_update_interval", () -> plugin.getConfig().getInt("selection-update-interval"));
+        addBooleanChart("top_bottom_cuboid", () -> plugin.getConfig().getBoolean("cuboid-top-bottom"));
         addCustomChart("worldedit_version", () -> "WorldEdit " + compatHelper.getWorldEditVersion());
         addBooleanChart("use_fawe", compatHelper::isUsingFawe);
         addBooleanChart("check_for_axe", () -> plugin.getConfig().getBoolean("need-we-wand"));
